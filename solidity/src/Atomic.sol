d0b8974bb1c2b0700133a55d3186f75eb404279d
Patricio Calderon <patricio.calderon@sansano.usm.cl>
2023-07-08 19:11:56 +0200

    feat(sol): add proof verification and test

--- solidity/src/Atomic.sol
+++ solidity/src/Atomic.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
-import "./Signature.sol";
+import "openzeppelin/utils/cryptography/ECDSA.sol";
+import "openzeppelin/utils/cryptography/SignatureChecker.sol";
 
-contract Atomic is VerifySignature {
+
+contract Atomic {
+    using ECDSA for bytes32;    
+
     struct SwapInfo {
         address from;
         address to;
@@ -21,7 +25,7 @@ contract Atomic is VerifySignature {
     mapping(uint256 => SwapInfo) public swaps;
     mapping(uint256 => uint256) public starts;
 
-    function propose(SwapInfo memory info) public payable {
+    function propose(SwapInfo memory info) public payable returns (uint256) {
         require(msg.sender == info.from, "Only the owner can propose");
         require(msg.value == info.amount, "Invalid amount");
 
@@ -29,21 +33,20 @@ contract Atomic is VerifySignature {
 
         swaps[id] = info;
         starts[id] = block.timestamp;
+
+        return id;
     }
 
     function settle(uint256 id, SwapProof memory proof, bytes memory signature) public {
         SwapInfo memory swap = swaps[id];
 
-        require(swap.amount > proof.amount, "Invalid amount");
+        require(swap.amount >= proof.amount, "Invalid settle amount");
 
         if(msg.sender == swap.from) {
             require(starts[id] + swap.duration < block.timestamp, "Invalid call timing");
         } else if(msg.sender == swap.to) {
-            // add code here to verify proof
             bytes32 hash = keccak256(abi.encode(proof));
-            address signer = recoverSigner(hash, signature);
-            require(signer == swap.from, "Invalid signature");
-
+            SignatureChecker.isValidSignatureNow(swap.from, hash, signature);
         } else {
             revert("Invalid caller");
         }
